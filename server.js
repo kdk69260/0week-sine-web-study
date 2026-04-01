7f0a62be5774dc3214d0d5ea6543c5394fd08ec1
kdk69260 <ubuntu@ip-172-31-29-178.ap-southeast-2.compute.internal>
2026-04-01 14:18:41 +0000

    fix: profile UI CSS 적용

--- server.js
+++ server.js
@@ -1,101 +1,89 @@
 const express = require("express");
-const app = express();
-const sqlite3 = require("sqlite3").verbose();
 const session = require("express-session");
-const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const path = require("path");
 
+const app = express();
 
-const db = new sqlite3.Database("users.db");
+// 미들웨어
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  secret: "secret-key",
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-db.run(`
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT,
-  password TEXT
-)
-`);
-
+// 홈
 app.get("/", (req, res) => {
-  res.send(`<link rel="stylesheet" href="/style.css">
-    <h1>홈</h1>
-    <a href="/signup">회원가입</a><br>
-    <a href="/login">로그인</a>
-  `);
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
+// 회원가입 페이지
 app.get("/signup", (req, res) => {
-  res.send(`<link rel="stylesheet" href="/style.css">
-    <h1>회원가입</h1>
-    <form method="POST">
-      아이디: <input name="username" /><br>
-      비밀번호: <input name="password" type="password"/><br>
-      <button>가입</button>
-    </form>
-  `);
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
 });
 
-app.post("/signup", async (req, res) => {
+// 회원가입 처리
+app.post("/signup", (req, res) => {
   const { username, password } = req.body;
-  const hashed = await bcrypt.hash(password, 10);
-
-  db.run("INSERT INTO users (username, password) VALUES (?, ?)", 
-    [username, hashed], 
-    () => {
-      res.redirect("/login");
-    }
-  );
+  req.session.user = { username, password };
+  res.redirect("/profile");
 });
 
+// 로그인 페이지
 app.get("/login", (req, res) => {
-  res.send(`<link rel="stylesheet" href="/style.css">
-    <div class="container">
-        <h1>로그인</h1>
-        <form method="POST">
-          <input type="text" placeholder="아이디"><br>
-          <input type="password" placeholder="비밀번호"><br>
-          <button>로그인</button>
-        </form>
-    </div>
-  `);
+  res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
+// 로그인 처리
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  db.get("SELECT * FROM users WHERE username = ?", [username], async (err, user) => {
-    if (!user) return res.send("유저 없음");
-
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.send("비밀번호 틀림");
-
-    req.session.user = user;
+  if (
+    req.session.user &&
+    req.session.user.username === username &&
+    req.session.user.password === password
+  ) {
     res.redirect("/profile");
-  });
+  } else {
+    res.send("로그인 실패");
+  }
 });
 
+// 프로필 페이지
 app.get("/profile", (req, res) => {
-  if (!req.session.user) return res.redirect("/login");
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
 
   res.send(`
-    <h1>프로필</h1>
-    <p>아이디: ${req.session.user.username}</p>
-    <a href="/logout">로그아웃</a>
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <link rel="stylesheet" href="/style.css">
+    </head>
+    <body>
+      <div class="container">
+        <h1>프로필</h1>
+        <p>아이디: ${req.session.user.username}</p>
+        <a href="/logout">로그아웃</a>
+      </div>
+    </body>
+    </html>
   `);
 });
 
+// 로그아웃
 app.get("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
 });
 
+// 서버 실행
 app.listen(3000, () => {
-  console.log("http://localhost:3000");
+  console.log("http://localhost:3000 실행됨");
 });
